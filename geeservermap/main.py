6ad71a6c1055f4e104ee533763f9614ceac5a7aa
Rodrigo Principe <rodrigo.principe@ldc.com>
2023-10-29 19:49:57 +0100

    feat: map's css as a block to allow interaction when routing

--- geeservermap/main.py
+++ geeservermap/main.py
@@ -9,15 +9,23 @@ MESSAGES = {}
 
 # load_dotenv()  # Load environment variable from .env
 PORT = 8018
+WIDTH = 800
+HEIGHT = 600
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--port', default=PORT, help='port')
+parser.add_argument('--port', default=PORT, help=f'Port in which the app will run. Defaults to {PORT}')
+parser.add_argument("--width", default=WIDTH, help=f"Width of the map's pane. Defaults to {WIDTH} px")
+parser.add_argument("--height", default=HEIGHT, help=f"Height of the map's pane. Defaults to {HEIGHT} px")
 
 app = Flask(__name__)
 
-@app.route("/")
-def map():
-    return render_template('map.html')
+
+def register_map(width, height):
+    """Register the index endpoint, allowing the user to pass a height and width"""
+    @app.route("/")
+    def map():
+        return render_template('map.html', width=width, height=height)
+
 
 @app.route('/add_layer', methods=['GET'])
 def add_layer():
@@ -34,17 +42,21 @@ def add_layer():
     MESSAGES[job_id] = layer
     return jsonify({'job_id': job_id})
 
+
 @app.route('/get_message', methods=['GET'])
 def get_message():
     job_id = request.args.get('id', type=str)
     return MESSAGES.get(job_id)
 
+
 @app.route('/messages')
 def messages():
     return jsonify(MESSAGES)
 
+
 def run():
     args = parser.parse_args()
     port = args.port
+    register_map(width=args.width, height=args.height)
     # webbrowser.open(f'http://localhost:{port}')
     app.run(debug=True, port=port)

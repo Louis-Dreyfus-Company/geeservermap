cef7c539df7b82fdc9f4470b29c51f207052d3df
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2023-11-14 14:17:45 +0000

    bump: version 0.0.0 → 0.0.0rc1

--- geeservermap/__init__.py
+++ geeservermap/__init__.py
@@ -6,6 +6,6 @@ from .elements import layers as layers
 from .main import MESSAGES as MESSAGES
 from .map import Map as Map
 
-__version__ = "0.0.0"
+__version__ = "0.0.0rc1"
 __author__ = "LDC Research Repository"
 __email__ = "remote-sensing@ldc.com"
